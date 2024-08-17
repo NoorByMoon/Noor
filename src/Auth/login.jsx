fd101416b44cc8316cf6c2cea04033a9b4e3bad7
Horlaide22 <hartoke2sekinat@gmail.com>
2024-08-17 22:11:24 +0100

    signin updated

--- src/Auth/login.jsx
+++ src/Auth/login.jsx
@@ -57,7 +57,7 @@ function Login() {
                 htmlFor="email"
                 className="block text-sm font-medium text-gray-700"
               >
-                 Email
+                Email
               </label>
               <input
                 type="text"
@@ -70,7 +70,7 @@ function Login() {
                 className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
               />
             </div>
-            <div className="relative">
+            <div className="">
               <label
                 htmlFor="password"
                 className="block text-sm font-medium text-gray-700"
@@ -79,30 +79,32 @@ function Login() {
                   <p className="text-red-500 text-sm">{passwordError}</p>
                 )}
               </label>
-              <input
-                type={visible ? "text" : "password"}
-                id="password"
-                placeholder="Enter password"
-                autoComplete="current-password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="mt-1 block w-full px-3 py-2 text-black bg-white border border-gray-300 rounded-md text-sm shadow-sm pr-10 placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-              />
-              <div className="absolute inset-y-5 right-2 flex items-center cursor-pointer">
-                {visible ? (
-                  <BsFillEyeSlashFill
-                    onClick={() => setVisible(false)}
-                    size={20}
-                    color="gray"
-                  />
-                ) : (
-                  <BsFillEyeFill
-                    onClick={() => setVisible(true)}
-                    size={20}
-                    color="gray"
-                  />
-                )}
+              <div className="relative">
+                <input
+                  type={visible ? "text" : "password"}
+                  id="password"
+                  placeholder="Enter password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="mt-1 block w-full px-3 py-2 text-black relative bg-white border border-gray-300 rounded-md text-sm shadow-sm pr-10 placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+                />
+                <div className="absolute inset-y-0 right-2 flex items-center cursor-pointer">
+                  {visible ? (
+                    <BsFillEyeSlashFill
+                      onClick={() => setVisible(false)}
+                      size={20}
+                      color="gray"
+                    />
+                  ) : (
+                    <BsFillEyeFill
+                      onClick={() => setVisible(true)}
+                      size={20}
+                      color="gray"
+                    />
+                  )}
+                </div>
               </div>
             </div>
             <div className=" ">
