1a18dab2218b7b50c082c179d3994a1ba3a635fd
Horlaide22 <hartoke2sekinat@gmail.com>
2024-08-20 14:53:13 +0100

    linked sign in and sign up tohether

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './Auth/login';
+import Login from './Auth/Login';
 import ForgetPassword from "./Auth/forgetPassword";
-import ResetPassword from './Auth/resetPassword';
+import ResetPassword from './Auth/ResetPassword';
 import RegistrationForm from "./Auth/RegistrationForm";
 
 function App() {
   return (
     <BrowserRouter className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/Sign-In" element={<Login />} />
         <Route path="/Forget-Password" element={<ForgetPassword />} />
         <Route path="/Reset-Password" element={<ResetPassword />} />
-        <Route path="/sign-up" element={<RegistrationForm />} />
+        <Route path="/Sign-Up" element={<RegistrationForm />} />
       </Routes>
     </BrowserRouter>
   );
