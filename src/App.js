a1b8d780064dee581f1a3428a9ab6719e47f55bb
Horlaide22 <hartoke2sekinat@gmail.com>
2024-09-02 07:25:06 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
-import Login from './Auth/login'
+// import Login from './Auth/login'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegistrationForm from "./Auth/RegistrationForm";
-import ForgetPassword from "./Auth/forgetPassword";
-import ResetPassword from './Auth/resetPassword';
+// import ForgetPassword from "./Auth/forgetPassword";
+// import ResetPassword from './Auth/resetPassword';
 import LandingPage from "./LandingPage"
 import Dashboard from "./Dashboard"
 
@@ -13,9 +13,9 @@ function App() {
     <BrowserRouter className="App">
       <Routes>
         <Route path="/" element={<RegistrationForm/>} />
-        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Login />} />
         <Route path="/Forget-Password" element={<ForgetPassword />} />
-        <Route path='/Reset-Password' element={<ResetPassword/>}/>
+        <Route path='/Reset-Password' element={<ResetPassword/>}/> */}
         <Route path='/Landing-page' element={<LandingPage/>}/>
         <Route path='/Dashboard' element={<Dashboard/>}/>                                        
       </Routes>
@@ -26,28 +26,4 @@ function App() {
 
 export default App;
 
-import Login from './Auth/login'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RegistrationForm from "./Auth/RegistrationForm";
-import ForgetPassword from "./Auth/forgetPassword";
-import ResetPassword from './Auth/resetPassword';
-import LandingPage from "./LandingPage"
-import Dashboard from "./Dashboard"
 
-
-function App() {
-  return (
-    <BrowserRouter className="App">
-      <Routes>
-        <Route path="/" element={<RegistrationForm/>} />
-        <Route path="/" element={<Login />} />
-        <Route path="/Forget-Password" element={<ForgetPassword />} />
-        <Route path='/Reset-Password' element={<ResetPassword/>}/>
-        <Route path='/Landing-page' element={<LandingPage/>}/>
-        <Route path='/Dashboard' element={<Dashboard/>}/>                                        
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
